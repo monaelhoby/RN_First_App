8c96ee99a64604532b7790e7f8885eb7b7930618
Mona Mohamed <42801687+monaelhoby@users.noreply.github.com>
2020-04-12 18:30:23 +0200

    Add files via upload

---
+++ src/screens/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Card, Block, Text} from '../shared'
+
+export default function About() {
+  return (
+    <Block >
+      <Card>
+        <Text text="About Screen" />
+      </Card>
+    </Block>
+  );
+}
